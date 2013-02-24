320dc7681eddf85f947efac5c4dec9e2cccfc163
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2013-02-24 04:39:17 +0100

    more params

--- notify.php
+++ notify.php
@@ -1,8 +1,13 @@
 <?php
 
-if (isset($_GET['file'])) {
-	$fp = fopen('file','w');
-	fwrite($fp, $_GET['file']);
+if (isset($_POST)) {
+
+	$action = $_POST['action'];
+	$user = $_POST['user'];
+	$file = $_POST['file'];
+
+	$fp = fopen('file','a');
+	fwrite($fp, $user . ' ' . $action . ' file ' . $file . "\n");
 	fclose($fp);
 }
 
